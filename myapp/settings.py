b0deaa447da11558cf4fe3f31ed3d93819ab692a
technica1121 <technica1121@docomo.ne.jp>
2018-08-18 17:10:55 +0900

    add,

--- myapp/settings.py
+++ myapp/settings.py
@@ -121,7 +121,9 @@ USE_TZ = False
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'))
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 import dj_database_url
 # DATABASES['default'] = dj_database_url.config()
