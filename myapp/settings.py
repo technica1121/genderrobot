aa8a34ecea5bc0a203e60259da684e8e8abc1205
technica1121 <technica1121@docomo.ne.jp>
2018-08-18 16:59:12 +0900

    # D

--- myapp/settings.py
+++ myapp/settings.py
@@ -124,7 +124,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),
 
 import dj_database_url
-#DATABASES['default'] = dj_database_url.config()
+# DATABASES['default'] = dj_database_url.config()
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
