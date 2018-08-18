dd8af0e2f68d5ff5a41dc3f13b47dee4ca7159c5
technica1121 <technica1121@docomo.ne.jp>
2018-08-18 16:56:02 +0900

    delete

--- myapp/settings.py
+++ myapp/settings.py
@@ -122,7 +122,6 @@ USE_TZ = False
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),
-#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 import dj_database_url
 #DATABASES['default'] = dj_database_url.config()
